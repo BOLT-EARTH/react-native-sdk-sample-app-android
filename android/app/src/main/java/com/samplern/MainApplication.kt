cccd6a83b1ca75cdfa5f9194d846ffd9a5a07a1a
Adil Khan <mohdadil.khan@bolt.earth>
2026-05-16 17:37:34 +0530

    Sample App RN for Android configuration.

--- android/app/src/main/java/com/samplern/MainApplication.kt
+++ android/app/src/main/java/com/samplern/MainApplication.kt
@@ -6,7 +6,9 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MainApplication : Application(), ReactApplication {
 
   override val reactHost: ReactHost by lazy {
